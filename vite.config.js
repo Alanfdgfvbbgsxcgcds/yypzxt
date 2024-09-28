3fe8e6c399433f337fc8fe069cf1ade6296f0f4f
jialiye <3359664028@qq.com>
2024-09-28 20:00:35 +0800

    shuji4

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 export default {
 
-  server: {
+  // server: {
 
-    proxy: {
-      // 匹配所有以 '/v3pz' 开头的请求
-      '/v3pz': {
-        target: 'https://v3pz.itndedu.com/', // 目标服务器
-        changeOrigin: true, // 推荐开启
-        rewrite: (path) => path.replace(/^\/v3pz/, '/v3pz'), // 重写路径
-      },
+  //   proxy: {
+  //     // 匹配所有以 '/v3pz' 开头的请求
+  //     '/v3pz': {
+  //       target: 'https://v3pz.itndedu.com/', // 目标服务器
+  //       changeOrigin: true, // 推荐开启
+  //       rewrite: (path) => path.replace(/^\/v3pz/, '/v3pz'), // 重写路径
+  //     },
 
-    },
-  },
+  //   },
+  // },
   base: '/yypzxt/',
   plugins: [
     vue(),
