a279ad6897458a2a05a72fabc97a4e0a5264f44f
jialiye <3359664028@qq.com>
2024-09-28 19:42:16 +0800

    ahaha3

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
 
 export default {
+
   server: {
-    port: 5500
+    port: 5500,
+    proxy: {
+      // 匹配所有以 '/v3pz' 开头的请求
+      '/v3pz': {
+        target: 'https://v3pz.itndedu.com/', // 目标服务器
+        changeOrigin: true, // 推荐开启
+        rewrite: (path) => path.replace(/^\/v3pz/, '/v3pz'), // 重写路径
+      },
+
+    },
   },
   base: '/yypzxt/',
   plugins: [
